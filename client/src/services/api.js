6c8e1352539597f7859ffa24cd75c43350344623
praveen00219 <paru2192000@gmail.com>
2025-11-12 12:47:10 +0530

    Updated API URL (Backend) to use production endpoint

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.MODE === 'development'
-  ? 'http://localhost:5000/api'
-  : '/api';
+// const API_URL = import.meta.env.MODE === 'development'
+//   ? 'http://localhost:5000/api'
+//   : '/api';
+
+const API_URL = "https://backend-subscription-dashboard-task.onrender.com"
 
 const api = axios.create({
   baseURL: API_URL,
